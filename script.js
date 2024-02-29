e7e022177a2971f8e6b124a8b2cbae5bc7a3dc3b
majidsaqr12 <smagedmohamed@gmail.com>
2024-02-29 09:04:30 +0200

    update

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-window.onscroll = function() {
-    var header = document.querySelector('nav');
-    if (window.scrollY > 50) {
-        header.classList.add('bg-dark'); // Change to desired class on scroll
-        header.classList.remove('bg-light');
-    } else {
-        header.classList.remove('bg-dark');
-        header.classList.add('bg-light');
-    }
-};
+$(document).ready(function(){
+    // Show or hide the sticky footer button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) { // Adjust this value according to your needs
+            $('#scrollTopBtn').fadeIn(200);
+        } else {
+            $('#scrollTopBtn').fadeOut(200);
+        }
+    });
+    
+    // Animate the scroll to top
+    $('#scrollTopBtn').click(function(event) {
+        event.preventDefault();
+        
+        $('html, body').animate({scrollTop: 0}, 300);
+    });
+});
+
 
 emailjs.init("YrXM6xr1JcO4UQCA3"); // Replace 'user_xxxxxx' with your user ID
 
 document.getElementById('myForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    const formData = {
-        fullname: this.fullname.value,
-        email: this.email.value,
-        phone: this.phone.value,
-    };
 
-    // These are the parameters that will be passed to your email template
+    // Use emailjs.sendForm to submit form data
     emailjs.sendForm('service_uehodo8', 'template_n13ol5f', this)
         .then(function(response) {
             console.log('SUCCESS!', response.status, response.text);
-            // You can add your success message or redirection here
+            
+            // Show success message to the user
+            alert('تم استلام طلبك بنجاح سنتواصل معك');
+
+            // Clear the form fields
+            document.getElementById('myForm').reset();
+            
         }, function(error) {
             console.log('FAILED...', error);
-            // You can add your failure message here
+
+            // Optionally, show an error message to the user
+            alert('Failed to send your form. Please try again.');
         });
 });
 
 document.getElementById('whatsappOrderBtn').addEventListener('click', function() {
     var phone = '+212770351416'; // Replace with your phone number
-    // var fullname = document.querySelector('input[name="fullname"]').value;
-    // var email = document.querySelector('input[name="email"]').value;
-    // var phoneUser = document.querySelector('input[name="phone"]').value;
-    
-    var message = encodeURIComponent("");
+    var message = encodeURIComponent("Your custom message here");
     var whatsappUrl = `https://wa.me/${phone}?text=${message}`;
     
     window.open(whatsappUrl, '_blank'); // Open WhatsApp chat in a new tab/window
